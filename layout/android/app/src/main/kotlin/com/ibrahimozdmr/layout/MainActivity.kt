1d803e82658a00229225b6c2532f087f57ef63c4
Halil İbrahim ÖZDEMİR <43646661+ibrahimozdmr@users.noreply.github.com>
2022-11-03 20:31:59 +0300

    odev3layout yüklendi

---
+++ layout/android/app/src/main/kotlin/com/ibrahimozdmr/layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ibrahimozdmr.layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ odev3layout/android/app/src/main/kotlin/com/ibrahimozdmr/odev3layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ibrahimozdmr.odev3layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
